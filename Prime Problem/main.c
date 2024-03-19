17b934f6a5b7e7960fdec7a05bb8adbae6fa644b
Tahawyy <101939990+Tahawyy@users.noreply.github.com>
2024-03-19 16:11:47 +0200

    Returned back X and R

--- Prime Problem/main.c
+++ Prime Problem/main.c
@@ -32,9 +32,15 @@ int main(int argc, char *argv[])
     if (my_rank != 0)
     {
         dest = 0;
-        MPI_Recv(&lb, 1, MPI_INT, dest, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // receive lower bound
-        MPI_Recv(&ub, 1, MPI_INT, dest, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // receive upper bound
-        for (int i = lb; i <= ub; i++) // count the number of primes in the range
+        if(my_rank == p - 1) // if the process is the last one        
+            MPI_Recv(&modu, 1, MPI_INT, dest, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // receive the remainder
+        MPI_Recv(&x, 1, MPI_INT, dest, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // receive lower bound
+        MPI_Recv(&r, 1, MPI_INT, dest, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // receive upper bound
+        lb = (my_rank - 1) * r + x;
+        ub = (my_rank * r) + x; // upper bound 
+        if (my_rank == p - 1) // if the process is the last one
+            ub += modu; // add the remainder to the upper bound 
+        for (int i = lb; i < ub; i++) // count the number of primes in the range
             if (isPrime(i))
                 cnt++;
         MPI_Send(&cnt, 1, MPI_INT, dest, 0, MPI_COMM_WORLD); // send the count to the master process
@@ -51,23 +57,18 @@ int main(int argc, char *argv[])
             printf("Total number of prime number in Master Process: %d\n", cnt);
             return 0;
         }
-        r = (y - x + 1) / (p - 1); // range for each process
-        modu = (y - x + 1) % (p - 1); // remainder
-        lb = x; // lower bound
+        r = (y - x) / (p - 1); // range for each process
+        modu = (y - x) % (p - 1); // remainder
+        
+        MPI_Send(&modu, 1, MPI_INT, p - 1, 0, MPI_COMM_WORLD); // send the remainder to the last process
         for (source = 1; source < p; source++) // send the range to each process
         {
-            range = r; // range for each process
-            MPI_Send(&lb, 1, MPI_INT, source, 0, MPI_COMM_WORLD); // send lower bound
-            if (source > (p - 1) - modu) // if there is a remainder
-                range++; // add one to the range
-            ub = lb + range - 1; // upper bound
-            MPI_Send(&ub, 1, MPI_INT, source, 0, MPI_COMM_WORLD); // send upper bound
-            lb = ub + 1; // update lower bound
+            MPI_Send(&x, 1, MPI_INT, source, 0, MPI_COMM_WORLD); // send the lower bound
+            MPI_Send(&r, 1, MPI_INT, source, 0, MPI_COMM_WORLD); // send the range
             MPI_Recv(&cnt, 1, MPI_INT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // receive the count of primes
             primes[source - 1] = cnt; // store the count in the array
             sum += primes[source - 1]; // add the count to the total
         }
-
         printf("Total number of prime numbers is: %d\n", sum); // print the total number of primes
         for (source = 1; source < p; source++) // print the number of primes in each process
             printf("Total number of prime number in P%d: %d\n", source, primes[source - 1]);
