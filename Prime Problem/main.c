41d415cfc247521854fe9cfb0c16e321648e1bcb
Tahawyy <101939990+Tahawyy@users.noreply.github.com>
2024-03-19 02:47:05 +0200

    added comments

--- Prime Problem/main.c
+++ Prime Problem/main.c
@@ -19,12 +19,8 @@ int main(int argc, char *argv[])
     int p;       /* number of process */
     int source;  /* rank of sender */
     int dest;    /* rank of reciever */
-    int x, y, r, lb, modu, ub, range, sum = 0;
-    int cnt = 0;
-
-    /* storage for  */
-
-    /* recieve */
+    int x, y, r, lb, modu, ub, range, sum = 0; 
+    int cnt = 0; /* count of primes */
 
     /* Start up MPI */
     MPI_Init(&argc, &argv);
@@ -32,50 +28,48 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     /* Find out number of process */
     MPI_Comm_size(MPI_COMM_WORLD, &p);
-    int primes[p];
+    int primes[p]; // array to store the number of primes in each process
     if (my_rank != 0)
     {
         dest = 0;
-        MPI_Recv(&lb, 1, MPI_INT, dest, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(&ub, 1, MPI_INT, dest, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        for (int i = lb; i <= ub; i++)
+        MPI_Recv(&lb, 1, MPI_INT, dest, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // receive lower bound
+        MPI_Recv(&ub, 1, MPI_INT, dest, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // receive upper bound
+        for (int i = lb; i <= ub; i++) // count the number of primes in the range
             if (isPrime(i))
                 cnt++;
-        MPI_Send(&cnt, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
+        MPI_Send(&cnt, 1, MPI_INT, dest, 0, MPI_COMM_WORLD); // send the count to the master process
     }
     else
     {
-        scanf("%d", &x);
-        scanf("%d", &y);
-        if (p == 1)
+        scanf("%d", &x); // input lower bound
+        scanf("%d", &y); // input upper bound
+        if (p == 1) // if there is only one process
         {
             for (int i = x; i <= y; i++)
-            {
                 if (isPrime(i))
-                    cnt++;
-            }
+                    cnt++;           
             printf("Total number of prime number in Master Process: %d\n", cnt);
             return 0;
         }
-        r = (y - x + 1) / (p - 1);
-        modu = (y - x + 1) % (p - 1);
-        lb = x;
-        for (source = 1; source < p; source++)
+        r = (y - x + 1) / (p - 1); // range for each process
+        modu = (y - x + 1) % (p - 1); // remainder
+        lb = x; // lower bound
+        for (source = 1; source < p; source++) // send the range to each process
         {
-            range = r;
-            MPI_Send(&lb, 1, MPI_INT, source, 0, MPI_COMM_WORLD);
-            if (source > (p - 1) - modu)
-                range++;
-            ub = lb + range - 1;
-            MPI_Send(&ub, 1, MPI_INT, source, 0, MPI_COMM_WORLD);
-            lb = ub + 1;
-            MPI_Recv(&cnt, 1, MPI_INT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            primes[source - 1] = cnt;
-            sum += primes[source - 1];
+            range = r; // range for each process
+            MPI_Send(&lb, 1, MPI_INT, source, 0, MPI_COMM_WORLD); // send lower bound
+            if (source > (p - 1) - modu) // if there is a remainder
+                range++; // add one to the range
+            ub = lb + range - 1; // upper bound
+            MPI_Send(&ub, 1, MPI_INT, source, 0, MPI_COMM_WORLD); // send upper bound
+            lb = ub + 1; // update lower bound
+            MPI_Recv(&cnt, 1, MPI_INT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // receive the count of primes
+            primes[source - 1] = cnt; // store the count in the array
+            sum += primes[source - 1]; // add the count to the total
         }
 
-        printf("Total number of prime numbers is: %d\n", sum);
-        for (source = 1; source < p; source++)
+        printf("Total number of prime numbers is: %d\n", sum); // print the total number of primes
+        for (source = 1; source < p; source++) // print the number of primes in each process
             printf("Total number of prime number in P%d: %d\n", source, primes[source - 1]);
     }
     /* shutdown MPI */
